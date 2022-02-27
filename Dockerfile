642e467a58cc112f12956cea28c27434422fdbd7
snowdence <snowdence2911@gmail.com>
2022-02-27 13:47:59 +0700

    [Add] Global Key

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ RUN addgroup -S spring && adduser -S spring -G spring
 USER spring:spring
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
+COPY cert /home/cert
 EXPOSE 8888
 ENTRYPOINT ["java","-jar","/app.jar"]
--- dev_script.bat
+++ dev_script.bat
@@ -1,3 +1,10 @@
 docker-compose down --rmi=all
 docker volume rm zeuswifosell_dbdata
 docker-compose up -d
+
+
+/*
+openssl pkcs12 -export -in ./certificate.crt -inkey ./private.key -name wifosell -out wifosell_com.p12
+keytool -importkeystore -deststorepass wifosellpass -destkeystore wifosell_com.jks -srckeystore wifosell_com.p12 -srcstoretype PKCS12
+
+*/
