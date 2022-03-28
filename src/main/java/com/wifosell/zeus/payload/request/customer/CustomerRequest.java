39a396a3415c4994362d955a58be775abec60039
Sang <enpro1955@gmail.com>
2022-03-28 14:26:11 +0700

    [Seeder] Customer

--- src/main/java/com/wifosell/zeus/payload/request/customer/CustomerRequest.java
+++ src/main/java/com/wifosell/zeus/payload/request/customer/CustomerRequest.java
@@ -1,13 +1,38 @@
 package com.wifosell.zeus.payload.request.customer;
 
+import com.wifosell.zeus.model.customer.Customer;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.validation.constraints.NotBlank;
+import java.util.Date;
 
 @Getter
 @Setter
 public class CustomerRequest {
     @NotBlank
     private String fullName;
+
+    private Date dob;   // Date of birth
+
+    @Enumerated(EnumType.ORDINAL)
+    private Customer.Sex sex;
+
+    private String phone;
+
+    private String email;
+
+    private String cin; // Citizen Identification Number
+
+    private String nation;
+
+    private String city;
+
+    private String district;
+
+    private String ward;
+
+    private String addressDetail;
 }
