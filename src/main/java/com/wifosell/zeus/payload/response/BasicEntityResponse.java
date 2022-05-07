7c672e49fba9acaae44ddf8e8836183b78d9b572
kieuconghau <kieuconghau2000@gmail.com>
2022-05-07 15:14:34 +0700

    fix json property of response from active to isActive

--- src/main/java/com/wifosell/zeus/payload/response/BasicEntityResponse.java
+++ src/main/java/com/wifosell/zeus/payload/response/BasicEntityResponse.java
@@ -1,5 +1,6 @@
 package com.wifosell.zeus.payload.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.wifosell.zeus.model.audit.BasicEntity;
 import lombok.Getter;
 
@@ -8,6 +9,7 @@ import java.time.Instant;
 @Getter
 public abstract class BasicEntityResponse {
     private final Long id;
+    @JsonProperty("isActive")
     private final boolean isActive;
     private final Instant createdAt;
     private final Instant updatedAt;
