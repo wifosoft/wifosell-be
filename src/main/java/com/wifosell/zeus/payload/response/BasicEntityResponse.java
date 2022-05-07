198ea8faf50c248e2e70e8484ae9325f0eef0fa8
kieuconghau <kieuconghau2000@gmail.com>
2022-05-07 15:36:30 +0700

    fix response isActive

--- src/main/java/com/wifosell/zeus/payload/response/BasicEntityResponse.java
+++ src/main/java/com/wifosell/zeus/payload/response/BasicEntityResponse.java
@@ -1,6 +1,5 @@
 package com.wifosell.zeus.payload.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.wifosell.zeus.model.audit.BasicEntity;
 import lombok.Getter;
 
@@ -9,10 +8,13 @@ import java.time.Instant;
 @Getter
 public abstract class BasicEntityResponse {
     private final Long id;
-    @JsonProperty("isActive")
     private final boolean isActive;
     private final Instant createdAt;
     private final Instant updatedAt;
+
+    public boolean getIsActive() {
+        return isActive;
+    }
 
     public BasicEntityResponse(BasicEntity entity) {
         this.id = entity.getId();
