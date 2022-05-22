58012731ce0afebab85442f12c94dead48bdc11d
kieuconghau <kieuconghau2000@gmail.com>
2022-05-22 15:26:27 +0700

    [ImportStockFromExcel] update response

--- src/main/java/com/wifosell/zeus/payload/response/stock/ImportStockTransactionResponse.java
+++ src/main/java/com/wifosell/zeus/payload/response/stock/ImportStockTransactionResponse.java
@@ -18,7 +18,6 @@ public class ImportStockTransactionResponse extends BasicEntityResponse {
     private final Supplier supplier;
     private final ImportStockTransaction.TYPE type;
     private final ImportStockTransaction.PROCESSING_STATUS processingStatus;
-    private final String source;
     private final String processingNote;
     private final List<ImportStockTransactionItemResponse> items;
 
@@ -27,7 +26,6 @@ public class ImportStockTransactionResponse extends BasicEntityResponse {
         this.warehouse = transaction.getWarehouse();
         this.supplier = transaction.getSupplier();
         this.type = transaction.getType();
-        this.source = transaction.getSource();
         this.processingStatus = transaction.getProcessingStatus();
         this.processingNote = transaction.getProcessingNote();
         this.items = transaction.getItems().stream().map(ImportStockTransactionItemResponse::new).collect(Collectors.toList());
