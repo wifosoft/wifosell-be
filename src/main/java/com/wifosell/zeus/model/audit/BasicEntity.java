3d9493b34628a825b7cc16a9ec5971cc2e1df6ea
snowdence <snowdence2911@gmail.com>
2022-06-16 22:49:59 +0700

    Check pom

--- src/main/java/com/wifosell/zeus/model/audit/BasicEntity.java
+++ src/main/java/com/wifosell/zeus/model/audit/BasicEntity.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
