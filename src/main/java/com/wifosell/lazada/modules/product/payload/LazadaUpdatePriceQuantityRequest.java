e5a7a1089410359f76a48fd7fc0ac3f6f3a76c5d
kieuconghau <kieuconghau2000@gmail.com>
2022-08-11 14:34:21 +0700

    [Maven] fix dependency

--- src/main/java/com/wifosell/lazada/modules/product/payload/LazadaUpdatePriceQuantityRequest.java
+++ src/main/java/com/wifosell/lazada/modules/product/payload/LazadaUpdatePriceQuantityRequest.java
@@ -1,6 +1,5 @@
 package com.wifosell.lazada.modules.product.payload;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
